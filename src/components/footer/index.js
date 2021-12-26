a19f8a95bce98126453b353260fb4e861b3810d4
abhisheksingh7455 <absingh7455@gmail.com>
2021-12-26 19:33:00 +0530

    first commit

---
+++ src/components/footer/index.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+           <b>
+            Contributed by 
+            <br></br>
+            <i> <a className="nav-link" href="https://www.linkedin.com/in/abhishek-singh-9244b8144/">
+                Abhishek Singh,{` `}{` `} {` `}
+            </a>
+            <a className="nav-link" href="https://in.linkedin.com/in/sakshi-shankar-533b27186">
+                Sakshi Shankar 
+            </a></i> </b>
+        </div>
+    )
+}
+
+export default Footer
